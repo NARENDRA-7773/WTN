a33a339e599035fed903a3df2156f3f15efb4fdf
NARENDRA-7773 <60385888+NARENDRA-7773@users.noreply.github.com>
2020-01-28 15:44:52 +0530

    Create code1.java

---
+++ code1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class CommandLineExample
+
+{
+
+ public static void main(String args[])
+
+ {
+
+  System.out.println(args[0]  +" technologies ");
+
+  System.out.println(args[1]);
+
+   }
+
+}
+
