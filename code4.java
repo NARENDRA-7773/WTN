d9fd74548ac73e7e93d65439776a0c189d341fef
NARENDRA-7773 <60385888+NARENDRA-7773@users.noreply.github.com>
2020-01-28 15:47:32 +0530

    Create code4.java

---
+++ code4.java
@@ -0,0 +1,11 @@
+import java.io.*;
+ class Sum
+{
+	public static void main(String args[])
+	{
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		int c=a+b;
+		System.out.println("the sum of 10 and 20 :"+c);		
+	}
+}
