136ba474dafa0b7f4fe630172c70819658d534f7
NARENDRA-7773 <60385888+NARENDRA-7773@users.noreply.github.com>
2020-01-28 15:53:22 +0530

    Create code5.java

---
+++ code5.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class Posneg{
+public static void main(String[]args)
+{
+int a=Integer.parseInt(args[0]);
+if(a>0)
+{
+System.out.println(a+" is Positive");
+}
+else
+{
+System.out.println(a+"is Negative");
+}
+}
+}
